8e2abb39a314c8e92ab1958b32b69bd0b3abd2fa
OneByteForLife <92474415+nameerror3301@users.noreply.github.com>
2022-12-12 20:49:44 +0200

    Удалил лишнее

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -15,20 +15,6 @@ func init() {
 	})
 }
 
-/*
-	<div class="p-uw-item__content" - Элемент в котором находятся все данные
-
-	<h1 class="p-uw-item__header" > font style="vertical-align: inherit;" - Название товара (TEXT)
-
-	<b class="c-price__price" > font style="vertical-align: inherit;" - Цена товара (TEXT)
-
-	<p class="p-uw-item__description" > font style="vertical-align: inherit;" - Описание товара (TEXT)
-
-	<a class="c-seller-info__name c-seller-info__name--link" > font class="vertical-align: inherit;" - Имя Пользователя (TEXT)
-
-	<div class="c-seller-info__date" > font class="vertical-align: inherit;" - Дата регистрации (TEXT)
-*/
-
 func main() {
 	t := time.Now()
 	app.Run()
